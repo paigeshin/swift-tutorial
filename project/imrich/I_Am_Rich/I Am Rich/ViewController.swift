ebc729826f7f6655d3258969589d2885d576b05a
shin <proscus0706@gmail.com>
2020-02-03 20:49:53 +0900

    first commit

---
+++ project/imrich/I_Am_Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by shin seunghyun on 2020/02/03.
+//  Copyright © 2020 shin. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    }
+
+
+}
