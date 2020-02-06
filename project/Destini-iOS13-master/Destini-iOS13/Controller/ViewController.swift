85e446518292ce66f1ad9489383d343a056a313a
shin <proscus0706@gmail.com>
2020-02-06 21:22:31 +0900

    pattern challenge complte

--- project/Destini-iOS13-master/Destini-iOS13/Controller/ViewController.swift
+++ project/Destini-iOS13-master/Destini-iOS13/Controller/ViewController.swift
@@ -14,20 +14,36 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
 
-
+    var storyBrain = StoryBrain()
+    var story: Story!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        updateUi()
-
+        //일단 처음에 스토리를 가져와서 화면에 보여준다.
+        story = storyBrain.getStory()
+        updateUi(story)
     }
 
-    func updateUi(){
-
-        
-
+    @IBAction func choice1Clicked(_ sender: UIButton) {
+        //일단 스토리 선택 값을 던지고 업데이트함
+        updateUi(story)
+        //다음
+        story = storyBrain.nextChoice1Story(story)
     }
-    
-    
+
+    @IBAction func choice2Clicked(_ sender: UIButton) {
+        //일단 스토리 선택 값을 던지고 업데이트함
+        updateUi(story)
+        //다음
+        story = storyBrain.nextChoice2Story(story)
+    }
+
+    func updateUi(_ story: Story) {
+        storyLabel.text = story.title
+        choice1Button.setTitle(story.choice1, for: .normal)
+        choice2Button.setTitle(story.choice2, for: .normal)
+    }
+
+
 }
 
